245745eba69b122a4e485d63c16a09ae73c9d04a
Sarath <sarathsaleem@gmail.com>
2015-05-11 02:09:43 +0400

    Add initial folders and files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/* global require, define, brackets: true, $, window, navigator */
+
+
+var GitDasboard = {};
