1e97455abcd4108c3936afc3bb5f48dcae3d1a91
Sarath <sarathsaleem@gmail.com>
2015-05-13 03:12:53 +0400

    Tree map exp

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ GitDasboard.ele = {};
 
 GitDasboard.getData = function () {
     $.getJSON('data/gitlog.json', function (data) {
-        GitDasboard.drawAreaChart(data, GitDasboard.ele.areaChart);
-        GitDasboard.drawScatterplot(data, GitDasboard.ele.scatterPlot);
-        GitDasboard.drawPieChart(data, GitDasboard.ele.pieChart);
+        //GitDasboard.drawAreaChart(data, GitDasboard.ele.areaChart);
+        //GitDasboard.drawScatterplot(data, GitDasboard.ele.scatterPlot);
+       // GitDasboard.drawPieChart(data, GitDasboard.ele.pieChart);
+        GitDasboard.drawTreeMap(data, GitDasboard.ele.treeMap);
 
     });
 };
@@ -19,12 +20,17 @@ GitDasboard.drawAreaChart = function (data, ele) {
 
     var canvasWidth = 1000, //$(canvas).width(),
         canvasHeight = 600,
-        margin = 30,
+        margin = 50,
         width = canvasWidth - 2 * margin,
         height = canvasHeight - 2 * margin;
 
+    
+    data.sort(function(a,b){
+      return new Date(b.date) - new Date(a.date);
+    });
+    
     data.forEach(function (d, i) {
-        d.date = new Date(d.date);
+        d.date = new Date(d.date); 
         d.index = data.length - i;
     });
 
@@ -105,10 +111,10 @@ GitDasboard.drawScatterplot = function (data, ele) {
 
 
     var x = d3.time.scale()
-        .range([0, width]);
+        .range([10, width]);
 
     var y = d3.scale.linear()
-        .range([height, 0]);
+        .range([height-10, 0]);
 
     x.domain(d3.extent(data, function (d) {
         return d.date;
@@ -220,6 +226,10 @@ GitDasboard.drawPieChart = function (data, ele) {
             commitsNumber[d.author_email] = 1;
         }
     });
+    
+    users.sort(function(a,b){
+       return commitsNumber[b] - commitsNumber[a];
+    });
 
     var arc = d3.svg.arc()
         .outerRadius(radius - 10)
@@ -270,11 +280,150 @@ GitDasboard.drawPieChart = function (data, ele) {
 };
 
 
+GitDasboard.drawTreeMap = function (data, ele) {
+    
+    var parent = {};
+    
+    function isInChildren(arrayOfObj, key , val) {
+        var isFound = null;
+        arrayOfObj.forEach(function (obj, index) {
+            if(obj[key] === val) {
+                isFound = index;
+            }
+        });
+        return isFound;
+    }
+    
+    var month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    
+    data.forEach(function (d) {    
+        d.date = new Date(d.date);
+        
+        var year = d.date.getFullYear(),
+            month = d.date.getMonth(),
+            day = d.date.getDate(),
+            hours = d.date.getHours();
+        
+        if (parent[year]) {            
+            var monthindex = isInChildren(parent[year].children, 'name' , month_names[month]);
+            
+            if (monthindex !== null) {
+                
+                var dayIndex = isInChildren(parent[year].children[monthindex].children, 'name' , day);
+                
+                if (dayIndex !== null) {
+                    
+                    parent[year].children[monthindex].children[dayIndex].size += 1;
+                    
+                } else {
+                    
+                    parent[year].children[monthindex].children.push( { name : day , size : 1 });
+                }
+                
+                
+            } else {                
+                parent[year].children.push({name : month_names[month], children : [] }); 
+            }       
+            
+        } else {
+            parent[year] = { name : year, children : [] };
+        }
+    });
+    
+    console.log(parent);
+    
+    /*
+    
+    {
+     name : 2010,
+     children : [
+     
+       { name : 0, children : [ {},{},{},{}]},
+       {}
+     
+     ]
+    }
+    */
+    
+    var diameter = 900,
+        margin = 30;
+    
+    
+    var color = d3.scale.linear()
+    .domain([-1, 5])
+    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+    .interpolate(d3.interpolateHcl);
+
+    var pack = d3.layout.pack()
+        .padding(2)
+        .size([diameter - margin, diameter - margin])
+        .value(function(d) { return d.size; })
+
+    var svg = d3.select(ele).append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+      .append("g")
+        .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+    
+    
+    var focus = parent,
+      nodes = pack.nodes(parent),
+      view;
+
+  var circle = svg.selectAll("circle")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+  var text = svg.selectAll("text")
+      .data(nodes)
+    .enter().append("text")
+      .attr("class", "label")
+      .style("fill-opacity", function(d) { return d.parent === parent ? 1 : 0; })
+      .style("display", function(d) { return d.parent === parent ? null : "none"; })
+      .text(function(d) { return d.name; });
+
+  var node = svg.selectAll("circle,text");
+
+  d3.select("body")
+      .style("background", color(-1))
+      .on("click", function() { zoom(parent); });
+
+  zoomTo([parent.x, parent.y, parent.r * 2 + margin]);
+
+  function zoom(d) {
+    var focus0 = focus; focus = d;
+
+    var transition = d3.transition()
+        .duration(d3.event.altKey ? 7500 : 750)
+        .tween("zoom", function(d) {
+          var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+          return function(t) { zoomTo(i(t)); };
+        });
+
+    transition.selectAll("text")
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+        .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+        .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+        .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function(d) { return d.r * k; });
+  }
+
+};
+
 //http://bl.ocks.org/mbostock/4063318 can be next
 
 $(function () {
     GitDasboard.ele.areaChart = $('.dashboard')[0];
     GitDasboard.ele.scatterPlot = $('.dashboard')[1];
     GitDasboard.ele.pieChart = $('.dashboard')[2];
+    GitDasboard.ele.treeMap = $('.dashboard')[3];
     GitDasboard.getData();
 });
